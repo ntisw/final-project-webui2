a8c60e57e4276ba1a154d9d6a70c29560cdd8760
Nantipat Suwanwong <scemisnis@gmail.com>
2019-05-13 17:44:33 +0700

    updated index page and considering about cookie for status login
    - index page :+1:
    - login.js :-1:
    - wacthman.js :confused:

---
+++ js/login/login.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $('#loginBtn').click(function(){
+    $(this).attr("disabled","disabled");
+    $('#labelLoading').toggleClass('sr-only');
+    $('#labelLogin').toggleClass('sr-only');
+    $('#loadingStatus').toggleClass('sr-only').toggle(1000,function(){
+        email=$('#inputEmail').val()
+        pass=$('#inputPassword').val()
+        if(email=='admin@nw.com'&&pass=='12345**'){
+          window.location.href='dashboard.html'
+        }else{
+          $('#divemail').effect('shake')
+          $('#divpass').effect('shake')
+          $('.invalid-feedback').show()
+        }
+        $('#loginBtn').removeAttr('disabled');
+        $('#labelLoading').toggleClass('sr-only');
+        $('#labelLogin').toggleClass('sr-only');
+        $(this).toggle().toggleClass('sr-only');
+    });
+  });
+  $('#inputEmail').keypress(function(){
+    $('.invalid-feedback').hide()
+  })
+});
